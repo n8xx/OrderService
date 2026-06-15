994147450487cb921f61cf65ad651a836be9792e
Natalia Baraukova <borovkova2088@gmail.com>
2026-06-15 05:37:40 +0300

    fix:add AbstractIntegrationTest

---
+++ src/test/java/com/innowise/orderservice/integration/AbstractIntegrationTest.java
@@ -0,0 +1,28 @@
+package com.innowise.orderservice.integration;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureWireMock(port = 0)
+public abstract class AbstractIntegrationTest {
+
+    @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16-alpine");
+
+    @DynamicPropertySource
+    static void registerProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+        registry.add("user-service.url", () -> "http://localhost:${wiremock.server.port}");
+    }
+}
